0ed78ece918ca4647e481df76ca4c517b8d0ada0
EternalNothingness <66483318+EternalNothingness@users.noreply.github.com>
2020-12-28 17:08:48 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+Titel: main
+Beschreibung:
+Autor: Patrick Wintner
+GitHub: https://github.com/EternalNothingness/FSST-5BHEL-Excercises-07_quicksort.git
+Datum der letzten Bearbeitung: 28.12.2020
+*/
+
+#include <stdlib.h>
+
+int main(){
+	// boot configuration
+}
